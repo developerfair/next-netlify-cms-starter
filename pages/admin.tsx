1f90be222fdc831840db0f4c636203e391d8c571
Justin <justin@developerfair.com>
2020-09-11 17:11:27 +0100

    Pretty preview

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,15 +5,9 @@ import { IndexPreview } from "../components/admin/IndexPreview";
 const AdminWithNoSSR = dynamic(
   () =>
     import("netlify-cms-app").then((CMS: any) => {
-      CMS.registerPreviewStyle("/styles/tailwind.css");
+      CMS.registerPreviewStyle("/admin/main.css");
       CMS.registerPreviewTemplate("index", IndexPreview);
-      CMS.init({
-        config: {
-          backend: {
-            name: "git-gateway",
-          },
-        },
-      });
+      CMS.init();
     }) as any,
   { ssr: false }
 );
